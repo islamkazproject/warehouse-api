7a613e62b9c2308855a841d39407c1a11d4eb43c
islam <islamkazproject@gmail.com>
2024-09-25 22:13:59 +0300

    Added metadata + naming convention

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -5,10 +6,13 @@ from sqlalchemy.orm import (
     mapped_column
 )
 
+from core.config import settings
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
     id: Mapped[int] = mapped_column(primary_key=True)
 
     @declared_attr.directive
